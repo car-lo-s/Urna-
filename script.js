2fd44d21ed8d9ca84d80ea8f4e4e1cb5f2f19eba
Carlos Renato <64719682+car-lo-s@users.noreply.github.com>
2022-07-11 12:46:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let seuVoto = document.querySelector(".tela-voto p")
+let cargo = document.querySelector(".tela-resultado p")
+let descricao =document.querySelector(".tela-pessoa")
+let aviso = document.querySelector(".tela-info")
+let foto = document.querySelector(".tela-direita")
+let num = document.querySelector(".tela-numero")
+
+function valor(n){
+    alert(n)
+}
+
+function branco(){
+    
+}
+
+function corrige(){
+    
+}
+
+function confirma(){
+    
+}
